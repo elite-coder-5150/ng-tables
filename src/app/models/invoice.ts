ff087e60188959e3cfbd590f072e44f7c64f8b0b
elite-coder-5150 <parkhousedarrell7@gmail.com>
2022-12-05 17:12:59 -0600

    added the customer interface used by the invoice

--- src/app/models/invoice.ts
+++ src/app/models/invoice.ts
@@ -1,2 +1,14 @@
+interface Customer {
+    name: string;
+    email: string;
+    password: string;
+    image: string;
+
+    address: string;
+}
 export interface Invoice {
+    invoice: number;
+    date: Date;
+    status: string;
+    customer: Customer;
 }
