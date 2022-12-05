28946cacff60a845de9679106249fb10f8d01cb8
elite-coder-5150 <parkhousedarrell7@gmail.com>
2022-12-05 15:14:12 -0600

    change the name of the selector

--- src/app/tables/tables.component.ts
+++ src/app/tables/tables.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-tables',
+  selector: 'ng-tables',
   templateUrl: './tables.component.html',
   styleUrls: ['./tables.component.scss']
 })
